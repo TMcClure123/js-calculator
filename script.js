209489b8e9bcb6afbf8b592a7ee46a16e5397a0a
TMcClure123 <jumpscares4days@gmail.com>
2023-10-03 13:28:19 -0400

    added copy and paste functions

--- script.js
+++ script.js
@@ -17,18 +17,22 @@ function mathButPress(operator) {
 function equalButPress() {
 
 }
+// clears everything except memory
 function clearButPress() {
 let preVal = ''
 let newVal = ''
 let resultVal= ''
 let mathOperator= ''
 let decimalClicked =  false
-let valMemStored = ''
 document.getElementsById('entry').value= '0'
 }
+// store the current value in #entry into valMemStored
 function copyButPress() {
-
+valMemStored = document.getElementsById('entry').value
 }
 function pasteButPress() {
-
+if (valMemStored) {
+    document.getElementById('entry').value = valMemStored
+    newVal = valMemStored    
+}
 }
