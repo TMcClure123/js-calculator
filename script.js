6688149237a71f93e1822dac78071ea7e3c843d9
TMcClure123 <jumpscares4days@gmail.com>
2023-10-03 13:04:40 -0400

    added basic js bars and functions

---
+++ script.js
@@ -0,0 +1,28 @@
+let preVal = ''
+let newVal = ''
+let resultVal= ''
+let mathOperator= ''
+// store wether or not decimal was clicked
+// (only allow one decimal per Val)
+let decimalClicked =  false
+// hold values we want stored in memory
+let valMemStored = ''
+
+function numButPress(num) {
+
+}
+function mathButPress(operator) {
+
+}
+function equalButPress() {
+
+}
+function clearButPress() {
+
+}
+function copyButPress() {
+
+}
+function pasteButPress() {
+    
+}
